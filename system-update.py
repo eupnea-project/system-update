fb65d2d20ab7f77e0eb9ac39467ae994f520148b
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-10 23:34:04 +0100

    Add functions.py

--- system-update.py
+++ system-update.py
@@ -2,5 +2,9 @@
 
 # This script is used to update Depthboot between releases.
 
+from functions import *
+
 if __name__ == "__main__":
     print("To-Do")
+    with open("/tmp/i-updated.txt", "w") as f:
+        f.write("1")
