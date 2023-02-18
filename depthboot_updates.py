3f00afe3796dfe1ee9f9888561c58631b7174540
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-18 02:25:10 +0100

    Try python thread

--- depthboot_updates.py
+++ depthboot_updates.py
@@ -5,6 +5,7 @@
 import contextlib
 import json
 import os
+import threading
 
 from functions import *
 
@@ -22,7 +23,8 @@ def start_update_service(packages_list: str) -> None:
     # bash aka subprocess.check_output waits for the process to finish
     # subprocess uses /bin/sh which does not support the & operator
     # -> use Popen and start it in a new process group
-    subprocess.Popen(["systemctl", "start", "eupnea-update.service"], preexec_fn=os.setsid)
+    bash("systemctl start eupnea-update.service")
+    # subprocess.Popen(["systemctl", "start", "eupnea-update.service"], preexec_fn=os.setsid)
 
 
 def v1_1_0():
@@ -122,7 +124,12 @@ def v1_1_5():
         case "ubuntu":
             start_update_service("systemd-zram-generator")
         case "arch":
-            start_update_service("iio-sensor-proxy zram-generator")
+            background_thread = threading.Thread(target=start_update_service, args=("iio-sensor-proxy zram-generator",))
+            background_thread.daemon = True
+
+            # Start the thread
+            background_thread.start()
+            # start_update_service("iio-sensor-proxy zram-generator")
         case _:
             # Fedora & Pop!_OS already have zram. Debian doesnt have systemd-generator-packages
             pass
